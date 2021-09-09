cc85c7c8673ed405e914df981661a7aa9f4f0eab
Felix Indrawan <indrawan.felix123@gmail.com>
2021-09-09 14:32:38 -0400

    Testing play audio on join

--- run.py
+++ run.py
@@ -1,3 +1,6 @@
+from asyncio import sleep
+
+import audioread
 import discord
 from discord.ext.commands import Bot
 import os
@@ -6,6 +9,8 @@ from dotenv import load_dotenv
 load_dotenv()
 TOKEN = os.getenv("DISCORD_BOT_TOKEN")
 bot = Bot(command_prefix='$', help_command=None)
+
+FFMPEG_PATH = "ffmpeg-20200831-4a11a6f-win64-static/bin/ffmpeg.exe"
 
 @bot.event
 async def on_ready():
@@ -33,6 +38,18 @@ async def help(ctx):
                         color=0x800080)
     await ctx.send(embed=embed)
 
+async def play_audio(channel_id):
+    currUserVC = bot.get_channel(channel_id)
+    audioPath = "sfx/theme.mp3"
+
+    vc = await currUserVC.connect()
+    await sleep(.5)
+    vc.play(discord.FFmpegPCMAudio(executable=FFMPEG_PATH, source=audioPath))
+    with audioread.audio_open(audioPath) as f:
+        # Start Playing
+        await sleep(f.duration)
+    await vc.disconnect()
+
 @bot.event
 async def on_voice_state_update(member, before, after):
     if ((before.channel != 'Vent' or before.channel != 'Secret Vent') and after.channel is not None):
@@ -42,8 +59,10 @@ async def on_voice_state_update(member, before, after):
         secret_amongus_channel_id = 885585481251835904
         if after.channel.id == vents_channel_id:
             await member.move_to(bot.get_channel(amongus_channel_id))
+            await play_audio(amongus_channel_id)
         if after.channel.id == secret_vents_channel_id:
-              await member.move_to(bot.get_channel(secret_amongus_channel_id))
+            await member.move_to(bot.get_channel(secret_amongus_channel_id))
+            await play_audio(amongus_channel_id)
 
 
 bot.run(TOKEN)
